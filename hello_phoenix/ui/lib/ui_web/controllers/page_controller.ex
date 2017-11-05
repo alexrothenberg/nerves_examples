92b4acaa54bb436a838510d26e71553a2b52db7a
Alex Rothenberg <alex@alexrothenberg.com>
2017-11-05 13:45:48 -0500

    some ui in phoenix

--- hello_phoenix/ui/lib/ui_web/controllers/page_controller.ex
+++ hello_phoenix/ui/lib/ui_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule UiWeb.PageController do
   use UiWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    led_mapping = StatusMonitor.get_led_mapping()
+    status = StatusMonitor.get_status()
+    render conn, "index.html", led_mapping: led_mapping, status: status
   end
 end
